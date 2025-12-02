322f321d50bf8dadf0a3270752bf24678a771f41
Caleb Gleason <167656229+HotCometGames@users.noreply.github.com>
2025-12-01 20:27:06 -0600

    Add AttackScript for handling attack behavior

---
+++ scripts/AttackScript.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackScript : MonoBehaviour
+{
+    public GameObject owner;
+
+    public int damage = 0;
+    public float attackDuration = 0.5f;
+    private float attackTimer = 0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        attackTimer += Time.deltaTime;
+        if (attackTimer >= attackDuration)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
