734bb5f676496b0500e6e1be355ce47cbce0bea0
Caleb Gleason <167656229+HotCometGames@users.noreply.github.com>
2025-12-17 21:56:17 -0600

    Add rotationOffset to AttackScript

--- scripts/AttackScript.cs
+++ scripts/AttackScript.cs
@@ -8,13 +8,15 @@ public class AttackScript : MonoBehaviour
 
     public int damage = 0;
     public float attackDuration = 0.5f;
+    public float rotationOffset = 0f;
     private float attackTimer = 0f;
+    
 
     public float spawnOffset = 0f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles + new Vector3(0, rotationOffset, 0));
     }
 
     // Update is called once per frame
