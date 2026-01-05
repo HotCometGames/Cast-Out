86872d9d8c92717f7d7b60e2d838d533036e3090
Caleb Gleason <167656229+HotCometGames@users.noreply.github.com>
2026-01-05 13:50:11 -0600

    Add ItemHoldingUIScript for item animation handling

---
+++ scripts/ItemHoldingUIScript.cs
@@ -0,0 +1,82 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ItemHoldingUIScript : MonoBehaviour
+{
+    [SerializeField] static private Animator animator;
+    [SerializeField] static private Image targetImage;
+    static public float playerSpeed = 0;
+    static private float useAnimationDuration = 0.4f;
+    static private float useAnimationTimer = 0;
+    void Awake()
+    {
+        animator = GetComponent<Animator>();
+        targetImage = GetComponent<Image>();
+    }
+    void Update()
+    {
+        if (useAnimationTimer > 0)
+        {
+            useAnimationTimer -= Time.deltaTime;
+            if (useAnimationTimer <= 0)
+            {
+                useAnimationTimer = 0;
+                if (playerSpeed > 0)
+                    PlayWalkAnimation();
+                else
+                    PlayIdleAnimation();
+            }
+        }
+        else
+        {
+            if (playerSpeed > 0)
+            {
+                PlayWalkAnimation();
+            }
+            else
+            {
+                PlayIdleAnimation();
+            }
+        }
+    }
+
+    static public void SetSprite(Sprite sprite)
+    {
+        targetImage.enabled = true;
+        targetImage.sprite = sprite;
+    }
+
+    static public void ClearSprite()
+    {
+        targetImage.sprite = null;
+        targetImage.enabled = false;
+    }
+    static public void PlayUseAnimation()
+    {
+        animator.speed = 1;
+        animator.Play("ItemHoldingUse");
+        useAnimationTimer = useAnimationDuration;
+    }
+    static public void SetSpeed(float speed)
+    {
+        playerSpeed = speed;
+    }
+
+    void PlayIdleAnimation()
+    {
+        if (useAnimationTimer > 0)
+            return;
+        animator.speed = 1;
+        animator.Play("ItemHoldingIdle");
+    }
+
+    void PlayWalkAnimation()
+    {
+        if (useAnimationTimer > 0)
+            return;
+        animator.speed = playerSpeed;
+        animator.Play("ItemHoldingWalking");
+    }
+}
