ec7f39d336c9912866f601f92e248c1be5a7f440
Caleb Gleason <167656229+HotCometGames@users.noreply.github.com>
2026-02-07 01:12:47 -0600

    Implement random glasses activation in OrcScript
    Added functionality to randomly activate glasses with a 20% chance.

--- scripts/OrcScript.cs
+++ scripts/OrcScript.cs
@@ -6,11 +6,21 @@ public class OrcScript : MonoBehaviour
 {
     public List<MultiTrade> possibleTrades;
     public List<Trade> trades;
-
+    [SerializeField] GameObject glasses;
     void Start()
     {
         int index = Random.Range(0, possibleTrades.Count);
         trades = possibleTrades[index].trades;
+
+        int glassesChance = Random.Range(0, 100);
+        if (glassesChance < 20) // 20% chance to spawn with glasses
+        {
+            glasses.SetActive(true);
+        }
+        else
+        {
+            glasses.SetActive(false);
+        }
     }
 
 }
