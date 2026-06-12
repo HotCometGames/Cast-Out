d55d4107716b5f0b019999b873b19e62a5ae63b0
Caleb Gleason <167656229+HotCometGames@users.noreply.github.com>
2026-06-12 16:44:31 -0500

    Add getName method to ItemPickup class

--- scripts/ItemPickup.cs
+++ scripts/ItemPickup.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,4 +6,9 @@ using UnityEngine;
 public class ItemPickup : MonoBehaviour
 {
     public ItemData itemData;
+
+    public String getName()
+    {
+        return itemData.name;
+    }
 }
