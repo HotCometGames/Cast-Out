4d48f33344ff8b221369fa15ab9b4600d04f1b6f
Caleb Gleason <167656229+HotCometGames@users.noreply.github.com>
2026-06-12 16:38:41 -0500

    Add DarkBallScript for dark attack functionality

---
+++ scripts/DarkBallScript.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DarkBallScript : MonoBehaviour
+{
+    [SerializeField]
+    GameObject darkAttackPrefab;
+    public float speed = 50f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Rigidbody rb = GetComponent<Rigidbody>();
+        rb.velocity = transform.forward * speed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.layer == LayerMask.NameToLayer("Ground"))
+        {
+            GameObject attack = Instantiate(darkAttackPrefab, transform.position, Quaternion.identity);
+            attack.GetComponent<AttackScript>().owner = GetComponent<AttackScript>().owner;
+            Destroy(gameObject);
+        }
+    }
+}
