0d4d0fefdeb2d245cc9a7bea47f4feb2622fde8a
Caleb Gleason <167656229+HotCometGames@users.noreply.github.com>
2026-06-12 16:39:15 -0500

    Add OnTriggerEnter method to handle collisions

--- scripts/FireballScript.cs
+++ scripts/FireballScript.cs
@@ -4,14 +4,19 @@ using UnityEngine;
 
 public class FireBallScript : MonoBehaviour
 {
-    public float maxDistance = 25f;
     public float speed = 50f;
-    private Vector3 startPosition;
     // Start is called before the first frame update
     void Start()
     {
-        startPosition = transform.position;
         Rigidbody rb = GetComponent<Rigidbody>();
         rb.velocity = transform.forward * speed;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.layer == LayerMask.NameToLayer("Ground"))
+        {
+            Destroy(gameObject);
+        }
     }
 }
